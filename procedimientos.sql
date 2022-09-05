a539202d53e72b641c1bfaa5342847f2b9905390
Sebasberacu <sebasberacu@gmail.com>
2022-09-04 20:02:33 -0600

    CRUD Proveedores
    limpié un toque el query y cambié la tabla de proveedores para que tenga el mismo "estilo" que cliente y producto

--- procedimientos.sql
+++ procedimientos.sql
@@ -38,7 +38,7 @@ SELECT Create_Cliente(87431,'Sebastian',array['Bermudez','Chacon'],
 				 array['8115153','60487716']);--Sirve
 select * from Cliente;
 
-
+----------------------------------------------------------------------
 
 --Read Cliente
 CREATE FUNCTION Read_Cliente (idCliente_ int, cedula int) 
@@ -59,32 +59,7 @@ select Read_Cliente(2,1);--Sirve
 select Read_Cliente(20,1);--Sirve
 select * from Cliente;
 
-
-
---Update Cliente
-CREATE FUNCTION Update_Cliente (idCliente_ int, numeroCedula integer,
-                            nombre varchar(20), apellidos varchar(20) array[3],
-                            fechaNacimiento date, correos varchar(100) array[2],
-                            telefonos varchar(10) array[2]) 
-RETURNS cliente AS $funcion_Update_Cliente$
-   DECLARE
-      clienteModificado Cliente;
-   BEGIN
-        --Validar nulos
-        if (numeroCedula is null and idCliente_ is null) then
-            raise notice 'Error, debe ingresar idCliente o numeroCedula para consultar datos de cliente.';
-        else                                            --si el primer argumento es null retorna el segundo
-            UPDATE Cliente set datosPersonales.nombre = COALESCE(nombre  ,datosPersonales.nombre),
-                    datosPersonales.apellidos = COALESCE(apellidos  ,datosPersonales.apellidos),
-                    datosPersonales.fechaNacimiento = COALESCE(fechaNacimiento  ,datosPersonales.fechaNacimiento),
-                    datosPersonales.correos = COALESCE(correos  ,datosPersonales.correos),
-                    datosPersonales.telefonos = COALESCE(telefonos  ,datosPersonales.telefonos)
-                from Cliente where Cliente.idCliente = idCliente_ or (datosPersonales).numeroCedula = numeroCedula;
-        end if;
-        
-    select * from Cliente where (datosPersonales).numeroCedula=numeroCedula into clienteIngresado;
-    RETURN clienteModificado;
-   END; $funcion_Update_Cliente$ LANGUAGE plpgsql;
+----------------------------------------------------------------------
 
 --Update Cliente
 CREATE FUNCTION Update_Cliente (idCliente_ int, numeroCedula integer,
@@ -119,6 +94,8 @@ select * from Update_Cliente(null,1,'Mauricio',null,null, null,null);--Sirve, no
 select * from Update_Cliente(1,null,'Mauricio',null,null, null,null);--Sirve, modificar nombre con el idCliente ingresado.
 select * from Update_Cliente(null,87431,'Carlos',null,null, null,null);--Sirve, modificar nombre con el numeroCedula ingresado.
 
+----------------------------------------------------------------------
+
 --Delete Cliente
 CREATE FUNCTION Delete_Cliente (idCliente_ int, numeroCedula integer) 
 RETURNS cliente AS $funcion_Delete_Cliente$
@@ -140,6 +117,10 @@ RETURNS cliente AS $funcion_Delete_Cliente$
 select * from Cliente;
 select * from Delete_Cliente(null,null);--Sirve, no hace nada solo muestra mensaje
 select * from Delete_Cliente(1,null);--Sirve, seleciona el cliente que borra antes de borrarlo para mostarlo.
+
+----------------------------------------------------------------------
+----------------------------------------------------------------------
+
 --Sebastian
     --CRUD Productos y clientes
 
@@ -168,7 +149,6 @@ AS $BODY$
 	   ) then
 		RAISE NOTICE 'Error, debe ingresar correctamente todos los datos.';
 	else
-		RAISE NOTICE 'ENTRO al else XDXDX';
 		INSERT INTO public.productos(producto)
 			SELECT(in_cantidad, in_precio, in_nombre, in_autores, in_descripcion, in_categorias)::producto
 			WHERE NOT EXISTS(SELECT 1 FROM productos where (producto).nombre = in_nombre); 
@@ -177,7 +157,6 @@ AS $BODY$
 	end if;
 	
 	select (producto).nombre from Productos where (producto).nombre = in_nombre into productoIngresado;
-	RAISE NOTICE 'LO SELECCIONA EN PRODUCTOINGRESADO';
 	return productoIngresado;
 	END; 
 $BODY$;
@@ -190,6 +169,8 @@ SELECT public.createproducto( --prueba sirve
 	array['muy pichudo'], 
 	array['rock','pop','clasica']
 );
+
+----------------------------------------------------------------------
 
 --READ PRODUCTO
 CREATE OR REPLACE FUNCTION public.readproducto( --:)
@@ -208,6 +189,8 @@ AS $BODY$
 		RETURN productoSeleccionado;
 	END; 
 $BODY$;
+
+----------------------------------------------------------------------
 
 --UPDATE PRODUCT
 CREATE FUNCTION updateProducto(
@@ -243,6 +226,8 @@ CREATE FUNCTION updateProducto(
 
 --si sirve, borré la prueba sin querer xd
 
+----------------------------------------------------------------------
+
 --DELETE PRODUCT
 CREATE OR REPLACE FUNCTION deleteProduct (in_idProduct int) 
 RETURNS Productos AS $function_deleteProduct$
@@ -253,7 +238,7 @@ RETURNS Productos AS $function_deleteProduct$
         if (in_idProduct is null) then
             raise notice 'Error, debe ingresar el id del producto para eliminarlo.';
 			return null;
-        else                                            --si el primer argumento es null retorna el segundo
+        else                                           
 			SELECT Productos.idProducto, Productos.producto from Productos
 			where Productos.idProducto = in_idProduct into productoEliminado;
             
@@ -265,6 +250,123 @@ RETURNS Productos AS $function_deleteProduct$
 
 select public.deleteproduct(10);
 
+----------------------------------------------------------------------
+----------------------------------------------------------------------
+
+--CREATE PROVEEDOR
+CREATE OR REPLACE FUNCTION public.createproveedor(
+	in_nombre character varying,
+	in_direccion Direccion,
+	in_productos producto[])
+    RETURNS character varying
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+AS $BODY$ 
+	DECLARE proveedoringresado character varying;
+	BEGIN
+	
+	if (in_nombre is null OR
+		in_direccion is null OR
+		in_productos is null
+	   ) then
+		RAISE NOTICE 'Error, debe ingresar correctamente todos los datos.';
+	else
+		INSERT INTO public.proveedores(datosProveedor)
+			SELECT(in_nombre, in_direccion, in_productos)::proveedor
+			WHERE NOT EXISTS(SELECT 1 FROM proveedores where (datosProveedor).nombre = in_nombre); 
+			--se valida con el nombre del proveedor, es un poco ineficiente pero tampoco es problema, no se van a repetir.
+
+	end if;
+	
+	select (datosProveedor).nombre from Proveedores where (datosProveedor).nombre = in_nombre into proveedoringresado;
+	return proveedoringresado;
+	END; 
+$BODY$;
+
+SELECT public.createproveedor(
+	'Santillana'::character varying, 
+	('San Jose','Desamparados','Desamparados','www.googlemaps.com/costarica/desamparados/desamparados')::direccion, 
+	array[(1,100,'Let It Be',array[('John','Paul')::autor],array['muy pichudo'],array['rock','salsa'])::producto]
+)
+
+----------------------------------------------------------------------
+
+--READ PRODUCTO
+CREATE OR REPLACE FUNCTION public.readproveedor(
+	in_idproveedor integer)
+    RETURNS proveedores
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+AS $BODY$
+	DECLARE
+		proveedorseleccionado Proveedores;
+	BEGIN
+		SELECT Proveedores.idProveedor, Proveedores.datosProveedor from Proveedores
+		where Proveedores.idProveedor = in_idproveedor into proveedorseleccionado;
+		
+		RETURN proveedorseleccionado;
+	END; 
+$BODY$;
+
+----------------------------------------------------------------------
+
+--UPDATE PROVEEDOR
+CREATE OR REPLACE FUNCTION updateProveedor(
+	in_idProveedor integer,
+	in_nombre character varying,
+	in_direccion Direccion,
+	in_productos producto[])
+	RETURNS Proveedores AS $function_updateProveedor$
+		DECLARE
+			proveedorActualizado Proveedores;
+		BEGIN
+			if (in_idProveedor is null) then
+				RAISE NOTICE 'Error, no ingresó id del producto';
+				return null;
+			else
+				UPDATE Proveedores set
+					datosproveedor.nombre = COALESCE(in_nombre,(datosproveedor).nombre),
+					datosproveedor.direccion = COALESCE(in_direccion,(datosproveedor).direccion),
+					datosproveedor.productos = COALESCE(in_productos,(datosproveedor).productos)
+				where Proveedores.idProveedor = in_idProveedor;
+				
+				SELECT Proveedores.idProveedor, Proveedores.datosProveedor from Proveedores
+				where Proveedores.idProveedor = in_idProveedor into proveedorActualizado;
+			END IF;
+			RETURN proveedorActualizado;
+		END; $function_updateProveedor$ LANGUAGE plpgsql;
+
+
+SELECT public.updateproveedor(
+	1, 
+	'Horacio'::character varying, 
+	('San Jose','Desamparados','Porvenir','www.googlemaps.com/costarica/desamparados/desamparados')::direccion, 
+	array[(1,100,'Let It Be',array[('John','Ringo')::autor],array['muy pichudo'],array['rock','salsa'])::producto]
+)
+
+----------------------------------------------------------------------
+
+--DELETE PROVEEDOR
+CREATE OR REPLACE FUNCTION deleteProveedor (in_idProveedor int) 
+RETURNS Proveedores AS $function_deleteProveedor$
+   DECLARE
+      proveedorEliminado Proveedores;
+   BEGIN
+        --Validar nulos
+        if (in_idProveedor is null) then
+            raise notice 'Error, debe ingresar el id del proveedor para eliminarlo.';
+			return null;
+        else                                           
+			SELECT Proveedores.idProveedor, Proveedores.datosProveedor from Proveedores
+			where Proveedores.idProveedor = in_idProveedor into proveedorEliminado;
+            
+			DELETE from Proveedores where Proveedores.idProveedor=in_idProveedor;
+
+        end if;
+   RETURN proveedorEliminado;
+   END;$function_deleteProveedor$ LANGUAGE plpgsql;
 
 --Juntos
     --Facturacion
